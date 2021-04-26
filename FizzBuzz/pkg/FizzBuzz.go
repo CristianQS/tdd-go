3cfbb842788d3bbd169dfbbc939e9759f66870bd
CristianQS <cristian_31teror@hotmail.com>
2021-04-27 00:40:07 +0100

    refactor: extract methods

--- FizzBuzz/pkg/FizzBuzz.go
+++ FizzBuzz/pkg/FizzBuzz.go
@@ -9,15 +9,23 @@ var (
 
 func Transform() [] string {
 	for i := 1 ; i <= 100; i++ {
-		if i % 3 == 0 {
+		if isFizz(i) {
 			result = append(result, "Fizz")
-		} else if i % 5 == 0 {
+		} else if isBuzz(i) {
 			result = append(result, "Buzz")
-		} else{
+		} else {
 			result = append(result, strconv.Itoa(i))
 		}
 	}
 	return result
+}
+
+func isBuzz(i int) bool {
+	return i%5 == 0
+}
+
+func isFizz(i int) bool {
+	return i%3 == 0
 }
 
 func GetNumber(number int) string {
