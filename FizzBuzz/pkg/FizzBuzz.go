6b603256768eca14c1cceeb703dddbe641a2476a
CristianQS <cristian_31teror@hotmail.com>
2021-04-27 00:45:50 +0100

    refactor: extract method and rename variable

--- FizzBuzz/pkg/FizzBuzz.go
+++ FizzBuzz/pkg/FizzBuzz.go
@@ -8,18 +8,22 @@ var (
 )
 
 func Transform() [] string {
-	for i := 1 ; i <= 100; i++ {
-		if isFizz(i) && isBuzz(i) {
+	for number := 1 ; number <= 100; number++ {
+		if isFizzBuzz(number) {
 			result = append(result, "FizzBuzz")
-		} else if isFizz(i) {
+		} else if isFizz(number) {
 			result = append(result, "Fizz")
-		} else if isBuzz(i) {
+		} else if isBuzz(number) {
 			result = append(result, "Buzz")
 		} else {
-			result = append(result, strconv.Itoa(i))
+			result = append(result, strconv.Itoa(number))
 		}
 	}
 	return result
+}
+
+func isFizzBuzz(i int) bool {
+	return isFizz(i) && isBuzz(i)
 }
 
 func isBuzz(i int) bool {
