85f7abb8c3d4ee16b20a7a53303d0141fb13bdaf
CristianQS <cristian_31teror@hotmail.com>
2021-05-31 21:49:42 +0100

    green: func Test_should_Normal_item_decrease_quality_by_one_when_sell_has_not_passed_out(t *testing.T) {

--- GildedRose/pkg/GildedRose_test.go
+++ GildedRose/pkg/GildedRose_test.go
@@ -6,9 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_should_decrease_quality_twice_as_fast_when_sell_passed_out(t *testing.T) {
+func Test_should_Normal_item_decrease_quality_by_one_when_sell_has_not_passed_out(t *testing.T) {
 	givenQuality := 10
-	item := &Item{name: "a", quality: givenQuality, sellIn: 0}
+	givenSellIn := 10
+	item := &Item{name: "AnItem", quality: givenQuality, sellIn: givenSellIn}
+	gildedRose := GildedRose{items: []*Item{item}}
+
+	gildedRose.UpdateQuality()
+
+	assert.Equal(t, givenQuality-1, item.quality)
+	assert.Equal(t, givenSellIn-1, item.sellIn)
+}
+
+func Test_should_Normal_item_decrease_quality_twice_as_fast_when_sell_passed_out(t *testing.T) {
+	givenQuality := 10
+	item := &Item{name: "AnItem", quality: givenQuality, sellIn: 0}
 	gildedRose := GildedRose{items: []*Item{item}}
 
 	gildedRose.UpdateQuality()
@@ -17,16 +29,15 @@ func Test_should_decrease_quality_twice_as_fast_when_sell_passed_out(t *testing.
 	assert.Equal(t, -1, item.sellIn)
 }
 
-func Test_should_not_decrease_quality_when_quality_is_zero(t *testing.T) {
+func Test_should_Normal_item_not_decrease_quality_when_quality_is_zero(t *testing.T) {
 	zeroQuality := 0
-	item := &Item{name: "a", quality: zeroQuality, sellIn: 0}
+	item := &Item{name: "AnItem", quality: zeroQuality, sellIn: 0}
 	gildedRose := GildedRose{items: []*Item{item}}
 
 	gildedRose.UpdateQuality()
 
 	assert.Equal(t, zeroQuality, item.quality)
 	assert.Equal(t, -1, item.sellIn)
-
 }
 
 func Test_should_Aged_Brie_increase_quality_when_sell_in_decrease(t *testing.T) {
