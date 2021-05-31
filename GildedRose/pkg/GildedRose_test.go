ec27b0a28b063c8867d4fa8fe5eaff8c3547bda1
CristianQS <cristian_31teror@hotmail.com>
2021-05-31 21:16:25 +0100

    green should_Aged_Brie_increase_quality_when_sell_in_decrease

--- GildedRose/pkg/GildedRose_test.go
+++ GildedRose/pkg/GildedRose_test.go
@@ -1,8 +1,9 @@
 package pkg
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_should_decrease_quality_twice_as_fast_when_sell_passed_out(t *testing.T) {
@@ -12,6 +13,24 @@ func Test_should_decrease_quality_twice_as_fast_when_sell_passed_out(t *testing.
 
 	gildedRose.UpdateQuality()
 
-	assert.Equal(t, givenQuality,item.quality)
+	assert.Equal(t, givenQuality-2, item.quality)
 }
 
+func Test_should_not_decrease_quality_when_quality_is_zero(t *testing.T) {
+	zeroQuality := 0
+	item := &Item{name: "a", quality: zeroQuality, sellIn: 0}
+	gildedRose := GildedRose{items: []*Item{item}}
+
+	gildedRose.UpdateQuality()
+
+	assert.Equal(t, zeroQuality, item.quality)
+}
+func Test_should_Aged_Brie_increase_quality_when_sell_in_decrease(t *testing.T) {
+	givenQuality := 10
+	item := &Item{name: "a", quality: givenQuality, sellIn: 10}
+	gildedRose := GildedRose{items: []*Item{item}}
+
+	gildedRose.UpdateQuality()
+
+	assert.Equal(t, givenQuality, item.quality+1)
+}
