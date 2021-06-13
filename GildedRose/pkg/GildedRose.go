725407cba9f954146f82e4b631aefcccb5becd2e
CristianQS <cristian_31teror@hotmail.com>
2021-06-13 22:21:27 +0100

    Restart :) Extract method AgedBrieUpdate

--- GildedRose/pkg/GildedRose.go
+++ GildedRose/pkg/GildedRose.go
@@ -6,69 +6,64 @@ type GildedRose struct {
 
 func (g *GildedRose) UpdateQuality() {
 	for i := 0; i < len(g.items); i++ {
-		if !IsAgedBrie(g, i) && !IsBackstage(g, i) {
-			if !(IsRagnaros(g.items[i])) {
-				NormalItemUpdate(g.items[i])
+		if g.items[i].name == "Aged Brie" {
+			AgedBrieUpdate(g.items[i])
+		}
+		if !(g.items[i].name == "Aged Brie") && !(g.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+			if g.items[i].quality > 0 {
+				if !(g.items[i].name == "Sulfuras, Hand of Ragnaros") {
+					g.items[i].quality = g.items[i].quality - 1
+				}
 			}
 		} else {
 			if g.items[i].quality < 50 {
-				AgedBrieItemUpdate(g.items[i])
 
-				if IsBackstage(g, i) {
-					BackstageItemUpdate(g.items[i])
+				if g.items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+					g.items[i].quality = g.items[i].quality + 1
+					if g.items[i].sellIn < 11 {
+						if g.items[i].quality < 50 {
+							g.items[i].quality = g.items[i].quality + 1
+						}
+					}
+
+					if g.items[i].sellIn < 6 {
+						if g.items[i].quality < 50 {
+							g.items[i].quality = g.items[i].quality + 1
+						}
+					}
 				}
 			}
 		}
 
-		if !(IsRagnaros(g.items[i])) {
+		if !(g.items[i].name == "Sulfuras, Hand of Ragnaros") {
 			g.items[i].sellIn = g.items[i].sellIn - 1
 		}
 
 		if g.items[i].sellIn < 0 {
-			if !IsAgedBrie(g, i) {
-				if !IsBackstage(g, i) {
-					NormalItemUpdate(g.items[i])
+			if !(g.items[i].name == "Aged Brie") {
+				if !(g.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+					if g.items[i].quality > 0 {
+						if !(g.items[i].name == "Sulfuras, Hand of Ragnaros") {
+							g.items[i].quality = g.items[i].quality - 1
+						}
+					}
 				} else {
 					g.items[i].quality = g.items[i].quality - g.items[i].quality
 				}
 			} else {
-				if g.items[i].quality < 50 {
-					g.items[i].quality = g.items[i].quality + 1
-				}
+
 			}
 		}
 	}
 }
 
-func IsBackstage(g *GildedRose, i int) bool {
-	return g.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-}
-
-func IsAgedBrie(g *GildedRose, i int) bool {
-	return g.items[i].name == "Aged Brie"
-}
-func IsRagnaros(item *Item) bool {
-	return item.name == "Sulfuras, Hand of Ragnaros"
-}
-
-func NormalItemUpdate(item *Item) {
-	if item.quality > 0 {
-		item.quality = item.quality - 1
+func AgedBrieUpdate(item *Item) {
+	if item.quality < 50 {
+		item.quality = item.quality + 1
 	}
-}
+	//g.items[i].sellIn = g.items[i].sellIn - 1
 
-func AgedBrieItemUpdate(item *Item) {
-	item.quality = item.quality + 1
-}
-
-func BackstageItemUpdate(item *Item) {
-	if item.sellIn < 11 {
-		if item.quality < 50 {
-			item.quality = item.quality + 1
-		}
-	}
-
-	if item.sellIn < 6 {
+	if item.sellIn < 0 {
 		if item.quality < 50 {
 			item.quality = item.quality + 1
 		}
