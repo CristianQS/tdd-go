688f9753dd1979ba1fe7350a5837c1c4b4985abe
CristianQS <cristian_31teror@hotmail.com>
2021-06-13 23:18:05 +0100

    Extract method NormalItemUpdate

--- GildedRose/pkg/GildedRose.go
+++ GildedRose/pkg/GildedRose.go
@@ -6,38 +6,27 @@ type GildedRose struct {
 
 func (g *GildedRose) UpdateQuality() {
 	for i := 0; i < len(g.items); i++ {
-		if g.items[i].name == "Aged Brie" {
+		if g.items[i].name == "Sulfuras, Hand of Ragnaros" {
+
+		} else if g.items[i].name == "Aged Brie" {
 			AgedBrieUpdate(g.items[i])
-		}
-		if g.items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+		} else if g.items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
 			BackstageUpdate(g.items[i])
+		} else {
+			NormalItemUpdate(g.items[i])
 		}
-		if !(g.items[i].name == "Aged Brie") && !(g.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-			if g.items[i].quality > 0 {
-				if !(g.items[i].name == "Sulfuras, Hand of Ragnaros") {
-					g.items[i].quality = g.items[i].quality - 1
-				}
-			}
-		}
+	}
+}
 
-		if !(g.items[i].name == "Sulfuras, Hand of Ragnaros") && !(g.items[i].name == "Aged Brie") &&
-			!(g.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-			g.items[i].sellIn = g.items[i].sellIn - 1
-		}
+func NormalItemUpdate(item *Item) {
+	if item.quality > 0 {
+		item.quality = item.quality - 1
+	}
+	item.sellIn = item.sellIn - 1
 
-		if g.items[i].sellIn < 0 {
-			if !(g.items[i].name == "Aged Brie") {
-				if !(g.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-					if g.items[i].quality > 0 {
-						if !(g.items[i].name == "Sulfuras, Hand of Ragnaros") {
-							g.items[i].quality = g.items[i].quality - 1
-						}
-					}
-				} else {
-				}
-			} else {
-
-			}
+	if item.sellIn < 0 {
+		if item.quality > 0 {
+			item.quality = item.quality - 1
 		}
 	}
 }
