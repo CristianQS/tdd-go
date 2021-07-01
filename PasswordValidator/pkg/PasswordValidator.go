9c5b905f436e2288c07daa0f4ce519793151f24c
CristianQS <cristian_31teror@hotmail.com>
2021-07-01 01:20:16 +0100

    should_password_not_pass_when_his_length_is_lower_than_8

--- PasswordValidator/pkg/PasswordValidator.go
+++ PasswordValidator/pkg/PasswordValidator.go
@@ -1,6 +1,7 @@
 package pkg
 
 func ValidatePassword(password string) bool {
+	if len(password) < 9 { return false}
 	return true
 }
 
--- PasswordValidator/pkg/PasswordValidator_test.go
+++ PasswordValidator/pkg/PasswordValidator_test.go
@@ -12,3 +12,11 @@ func Test_should_password_pass_the_rules(t *testing.T) {
 
 	assert.Equal(t, true,result)
 }
+
+func Test_should_password_not_pass_when_his_length_is_lower_than_8(t *testing.T) {
+	validatedPassword := "AsqWaOq8"
+
+	var result = ValidatePassword(validatedPassword)
+
+	assert.Equal(t, false,result)
+}
