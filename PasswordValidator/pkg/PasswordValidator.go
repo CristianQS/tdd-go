8e3cac038757aeecf22bb1629cf8cd082ff4af35
CristianQS <cristian_31teror@hotmail.com>
2021-07-02 14:22:21 +0100

    green: green: should_password_not_pass_when_there_is_not_capital_letter

--- PasswordValidator/pkg/PasswordValidator.go
+++ PasswordValidator/pkg/PasswordValidator.go
@@ -1,7 +1,10 @@
 package pkg
 
+import "strings"
+
 func ValidatePassword(password string) bool {
 	if len(password) < 9 { return false}
+	if password == strings.ToLower(password) { return false }
 	return true
 }
 
--- PasswordValidator/pkg/PasswordValidator_test.go
+++ PasswordValidator/pkg/PasswordValidator_test.go
@@ -20,3 +20,11 @@ func Test_should_password_not_pass_when_his_length_is_lower_than_8(t *testing.T)
 
 	assert.Equal(t, false,result)
 }
+
+func Test_should_password_not_pass_when_there_is_not_capital_letter(t *testing.T) {
+	validatedPassword := "asqwlaoq8"
+
+	var result = ValidatePassword(validatedPassword)
+
+	assert.Equal(t, false,result)
+}
