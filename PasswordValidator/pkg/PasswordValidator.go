f3c0e2cc032442b5d155ee67c1c40266eb662c95
CristianQS <cristian_31teror@hotmail.com>
2021-07-02 14:30:37 +0100

    Refactor: Extract methods

--- PasswordValidator/pkg/PasswordValidator.go
+++ PasswordValidator/pkg/PasswordValidator.go
@@ -4,13 +4,22 @@ import "strings"
 
 func ValidatePassword(password string) bool {
 	if HasMoreThan8Characters(password) { return false }
-	if password == strings.ToLower(password) { return false }
-	if password == strings.ToUpper(password) { return false }
+	if NotContainsUpperCase(password) { return false }
+	if NotContainsLowerCase(password) {	return false }
 	return true
 }
 
 func HasMoreThan8Characters(password string) bool {
 	return len(password) <= 8
 }
+
+func NotContainsUpperCase(password string) bool {
+	return password == strings.ToLower(password)
+}
+
+func NotContainsLowerCase(password string) bool {
+	return password == strings.ToUpper(password)
+}
+
 
 
