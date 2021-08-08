73408d586d09746b2202787eb141cb269c313796
CristianQS <cristian_31teror@hotmail.com>
2021-08-08 21:45:59 +0100

    green: should_alarm_be_deactivated_when_alarm_has_been_activated

--- TirePressureVariation/pkg/Alarm_test.go
+++ TirePressureVariation/pkg/Alarm_test.go
@@ -39,3 +39,17 @@ func Test_should_alarm_be_not_activated_when_sensor_is_between_threshold(t *test
 
 	assert.Equal(t, "",logger.GetMessage())
 }
+
+func Test_should_alarm_be_deactivated_when_alarm_has_been_activated(t *testing.T) {
+	sensor := &MockSensor{}
+	logger := &MockLogger{}
+	sensor.SetPressure(16)
+	alarm := NewAlarm(sensor,logger)
+
+	alarm.check()
+
+	sensor.SetPressure(20)
+	alarm.check()
+
+	assert.Equal(t, "Alarm deactivated!",logger.GetMessage())
+}
