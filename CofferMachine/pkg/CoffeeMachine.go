347179e0a606ac7b11cc7166335685cdafcdd414
CristianQS <cristian_31teror@hotmail.com>
2021-08-11 01:09:37 +0100

    red: should_create_a_tea_drink_with_no_sugar_command

---
+++ CofferMachine/pkg/CoffeeMachine.go
@@ -0,0 +1,18 @@
+package pkg
+
+type CoffeeMachine struct {
+	drinkMaker DrinkMaker
+}
+
+func NewCoffeeMachine(drinkMaker DrinkMaker) *CoffeeMachine {
+	return &CoffeeMachine{drinkMaker: drinkMaker}
+}
+
+
+
+func (c *CoffeeMachine) Execute() {
+
+
+}
+
+
--- CofferMachine/pkg/CoffeeMachine_test.go
+++ CofferMachine/pkg/CoffeeMachine_test.go
@@ -1,10 +1,16 @@
 package pkg
 
 import (
-	"github.com/stretchr/testify/assert"
+	"github.com/golang/mock/gomock"
 	"testing"
 )
 
-func Test_Fix_me_and_rename_me(t *testing.T) {
-	assert.Equal(t, false, true)
+func Test_should_create_a_tea_drink_with_no_sugar_command(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockDrinkMaker := NewMockDrinkMaker(ctrl)
+	var coffeeMachine = NewCoffeeMachine(mockDrinkMaker)
+
+	coffeeMachine.Execute()
+
+	mockDrinkMaker.EXPECT().execute(gomock.Eq("T::"))
 }
