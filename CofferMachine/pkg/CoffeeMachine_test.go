eb5461fcd81dcf2227c2dfbe0c81468b6744da34
CristianQS <cristian_31teror@hotmail.com>
2021-09-05 20:03:44 +0100

    Fix given values test

--- CofferMachine/pkg/CoffeeMachine_test.go
+++ CofferMachine/pkg/CoffeeMachine_test.go
@@ -28,9 +28,9 @@ func Test_should_create_a_coffee_drink_with_no_sugar_command(t *testing.T) {
 func Test_should_create_a_chocolate_drink_with_two_sugar_and_one_stick_command(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	mockDrinkMaker := NewMockDrinkMaker(ctrl)
-	var coffeeMachine = NewCoffeeMachine(mockDrinkMaker,&Drink{DrinkType: "Coffee", NumberSugar: 2})
+	var coffeeMachine = NewCoffeeMachine(mockDrinkMaker,&Drink{DrinkType: "Chocolate", NumberSugar: 2})
 
-	mockDrinkMaker.EXPECT().execute(gomock.Eq("C:2:0")).Times(1)
+	mockDrinkMaker.EXPECT().execute(gomock.Eq("H:2:0")).Times(1)
 
 	coffeeMachine.Execute()
 }
