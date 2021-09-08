071be4d83a93a6dcd9f02769c3dca8bd8a35c486
CristianQS <cristian_31teror@hotmail.com>
2021-09-09 00:36:57 +0100

    green: should_deposit_an_amount_of_money

--- Bank-Kata/pkg/Account.go
+++ Bank-Kata/pkg/Account.go
@@ -4,7 +4,12 @@ type Account struct {
 	balance int
 }
 
+func (a *Account) GetBalance() int {
+	return a.balance
+}
+
 func (a *Account) deposit(amount int) {
+	a.balance += amount
 }
 
 func (a *Account) withdraw(amount int) {
--- Bank-Kata/pkg/Account_test.go
+++ Bank-Kata/pkg/Account_test.go
@@ -3,6 +3,7 @@ package pkg
 import (
 	"bankkata/pkg/Mocks"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -21,3 +22,13 @@ func Test_should_deposit_and_withdraw_an_amount_of_money_and_print_it_in_console
 	console.EXPECT().Print(gomock.Eq("08/09/2021 | 300 | 300")).Times(1)
 	console.EXPECT().Print(gomock.Eq("08/09/2021 | -100 | 200")).Times(1)
 }
+
+func Test_should_deposit_an_amount_of_money(t *testing.T) {
+	givenADepositMoney := 300
+	account := Account{}
+
+	account.deposit(givenADepositMoney)
+
+	assert.Equal(t, givenADepositMoney, account.GetBalance())
+}
+
