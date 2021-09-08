015da0eaae27df44db4ff156be51a9a1ed1cc7dd
CristianQS <cristian_31teror@hotmail.com>
2021-09-09 00:34:14 +0100

    Fix acceptance test

---
+++ Bank-Kata/pkg/Account.go
@@ -0,0 +1,14 @@
+package pkg
+
+type Account struct {
+	balance int
+}
+
+func (a *Account) deposit(amount int) {
+}
+
+func (a *Account) withdraw(amount int) {
+}
+
+func (a *Account) printStatement() {
+}
--- Bank-Kata/pkg/Account_test.go
+++ Bank-Kata/pkg/Account_test.go
@@ -1,19 +1,23 @@
 package pkg
 
 import (
+	"bankkata/pkg/Mocks"
+	"github.com/golang/mock/gomock"
 	"testing"
 )
 
 func Test_should_deposit_and_withdraw_an_amount_of_money_and_print_it_in_console(t *testing.T) {
 	givenADepositMoney := 300
 	givenAWithdrawMoney := 100
+	account := Account{}
+	ctrl := gomock.NewController(t)
+	console := Mocks.NewMockLogger(ctrl)
 
-	AccountService{}.Deposit(givenADepositMoney)
-	AccountService{}.Withdraw(givenAWithdrawMoney)
-	AccountService{}.PrintStatement()
+	account.deposit(givenADepositMoney)
+	account.withdraw(givenAWithdrawMoney)
+	account.printStatement()
 
-	console.EXPECT().print("DATE | AMOUNT | BALANCE").Times(1)
-	console.EXPECT().print("08/09/2021 | 300 | 300").Times(1)
-	console.EXPECT().print("08/09/2021 | -100 | 200").Times(1)
-
+	console.EXPECT().Print(gomock.Eq("DATE | AMOUNT | BALANCE")).Times(1)
+	console.EXPECT().Print(gomock.Eq("08/09/2021 | 300 | 300")).Times(1)
+	console.EXPECT().Print(gomock.Eq("08/09/2021 | -100 | 200")).Times(1)
 }
