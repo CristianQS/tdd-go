dfd4e62808a99cf15dc2247aa410f9a2820ea87d
CristianQS <cristian_31teror@hotmail.com>
2021-10-23 22:36:07 +0100

    refactor: create table driven test

--- CofferMachine/pkg/CoffeeMachine_test.go
+++ CofferMachine/pkg/CoffeeMachine_test.go
@@ -5,24 +5,27 @@ import (
 	"testing"
 )
 
-func Test_create_tea_command_without_sugar(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	mockDrinkMaker := NewMockDrinkMaker(ctrl)
-	coffeeMachine := NewCoffeeMachine(mockDrinkMaker)
+func Test_create_drink_command_without_sugar(t *testing.T) {
+	tests := []struct{
+		name string
+		drinkOrder *DrinkOrder
+		expected string
+	}{
+		{name: "Tea", drinkOrder: NewDrinkOrder("T"), expected:"T::" },
+		{name: "Chocolate", drinkOrder: NewDrinkOrder("H"),expected:"H::" },
+	}
 
-	mockDrinkMaker.EXPECT().execute(gomock.Eq("T::")).Times(1)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			mockDrinkMaker := NewMockDrinkMaker(ctrl)
+			coffeeMachine := NewCoffeeMachine(mockDrinkMaker)
 
-	coffeeMachine.Execute(NewDrinkOrder("T"))
-}
+			mockDrinkMaker.EXPECT().execute(gomock.Eq(tc.expected)).Times(1)
 
-func Test_create_chocolate_command_without_sugar(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	mockDrinkMaker := NewMockDrinkMaker(ctrl)
-	coffeeMachine := NewCoffeeMachine(mockDrinkMaker)
-
-	mockDrinkMaker.EXPECT().execute(gomock.Eq("H::")).Times(1)
-
-	coffeeMachine.Execute(NewDrinkOrder("H"))
+			coffeeMachine.Execute(tc.drinkOrder)
+		})
+	}
 }
 
 
