f3b42932479f03c1eb061c241fc89e50a971dafc
CristianQS <cristian_31teror@hotmail.com>
2021-10-27 20:51:13 +0100

    green: Test_add_some_drink_order_to_repository

--- CofferMachine/pkg/repository/DrinkRepositoryInMemory_test.go
+++ CofferMachine/pkg/repository/DrinkRepositoryInMemory_test.go
@@ -6,10 +6,23 @@ import (
 	"testing"
 )
 
-func Test_add_an_order_to_repository(t *testing.T) {
+func Test_add_a_drink_to_repository(t *testing.T) {
 	repository := NewOrderRepositoryInMemory()
 	drink := model.NewDrink(model.Tea, 0.4)
 
 	repository.Add(drink)
 	assert.Equal(t,	repository.drinks[drink],1)
+}
+
+func Test_add_some_drink_to_repository(t *testing.T) {
+	repository := NewOrderRepositoryInMemory()
+	teaDrink := model.NewDrink(model.Tea, 0.4)
+	coffeeDrink := model.NewDrink(model.Coffee, 0.5)
+
+	repository.Add(teaDrink)
+	repository.Add(teaDrink)
+	repository.Add(coffeeDrink)
+
+	assert.Equal(t,	repository.drinks[teaDrink],2)
+	assert.Equal(t,	repository.drinks[coffeeDrink],1)
 }
